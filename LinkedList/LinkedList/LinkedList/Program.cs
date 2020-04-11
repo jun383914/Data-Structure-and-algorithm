34e03dc05b2f7d9c2d937cd5e961905c56a4ccde
Zhen <zhen.yang3@outlook.com>
2020-04-11 00:09:22 -0700

    Create Program.cs

---
+++ LinkedList/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create Linked List Class that can be accessed and instantiation with the first null value.
+            
+        }
+    }
+
+    public class Node
+    {
+    }
+
+    public class LinkedList
+    {
+        public int val;
+        public Node node;
+        public LinkedList()
+        {
+            this.node = null;
+        }
+     
+    }
+}
