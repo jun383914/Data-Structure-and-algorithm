05b10bdefa9c2503c88d08039c75ef6b0c4ad75a
jun383914 <zhen.yang3@outlook.com>
2020-05-13 11:01:46 -0700

    Update Program.cs
    Finish up InsertAfter and Delete methods

--- LinkedList Insertion/Linkedlist Insertion/Linkedlist Insertion/Program.cs
+++ LinkedList Insertion/Linkedlist Insertion/Linkedlist Insertion/Program.cs
@@ -14,8 +14,11 @@ namespace Linkedlist_Insertion
             linkedlist Linkedlist = new linkedlist();
             Linkedlist.AppendToLinkedList(3);
             Linkedlist.AppendToLinkedList(5);
-            //Linkedlist.AppendToLinkedList(8);
-            //Linkedlist.AppendToLinkedList(9);
+            Linkedlist.InsertBefore(5,4);
+            Linkedlist.InsertBefore(3,2);
+            Linkedlist.printLinkedList();
+            //Linkedlist.InsertBefore(1, 20);
+            Linkedlist.Delete(1);
             Linkedlist.printLinkedList();
         }
         //Extending LinkedList Class to append, insertBefore, insertAfter, and delete
@@ -67,7 +70,60 @@ namespace Linkedlist_Insertion
                 currentNode = currentNode._node;
             }
             result.Append("NULL");
+
             Console.WriteLine(result);
+        }
+        public void InsertBefore(int Beforethisvalue,int valuetobeinserted)
+        {
+            if(this.head._value==Beforethisvalue)
+            {
+                this.head = new Node(valuetobeinserted, this.head);
+                return;
+            }
+            Node currentNode = this.head;
+            while(currentNode._node!=null)
+            {
+                if(currentNode._node._value==Beforethisvalue)
+                {
+                    currentNode._node = new Node(valuetobeinserted, currentNode._node);
+                    return;
+                }
+
+                currentNode = currentNode._node;
+            }
+            throw new Exception("Cannot find the correct place to insert the value");
+        }
+        public void InsertAfter(int Afterthisvalue,int valuetobeinserted)
+        {
+            Node currentNode = this.head;
+                while(currentNode!=null)
+            { 
+                if (currentNode._value == Afterthisvalue)
+                {
+                    currentNode._node = new Node(valuetobeinserted, currentNode._node);
+                }
+                currentNode = currentNode._node;
+            }
+            throw new Exception("This value cannot be inserted");
+        }
+        public void Delete(int tobeDeleted)
+        {
+            Node currentNode = this.head;
+
+            if (this.head._value==tobeDeleted)
+            {
+                head = currentNode._node;
+            }
+            while(currentNode._node!=null)
+            {
+                if(currentNode._node._value==tobeDeleted)
+                {
+                    currentNode._node = currentNode._node._node;
+                    break;
+                }
+                currentNode = currentNode._node;
+            }
+            throw new Exception("value cannot be deleted because it doesn't exist in linkedlist");
         }
     }
 
